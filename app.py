0de5d50d94c2efebb581a06e9109f4747d4839c8
nathanbobai <85312755+nathanbobai@users.noreply.github.com>
2025-08-01 16:03:39 +0100

    Update with Vul app.py

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
+import os
+
 def hello():
     return "Hello, CI/CD!"
 
+def dangerous_code():
+    # Using eval() is a major security risk (arbitrary code execution)
+    user_input = "print('This is unsafe!')"
+    eval(user_input)  
+
 if __name__ == "__main__":
     print(hello())
+    dangerous_code()
